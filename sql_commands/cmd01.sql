b0feca62f1689ca1b370fe550164fecf1a3e35fd
henry440 <440henry@gmail.com>
2021-03-03 21:35:07 +0100

    Inizialisierung der DB

---
+++ sql_commands/cmd01.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "startentrys" (
+	"id"	INTEGER NOT NULL,
+	"timestamp"	INTEGER NOT NULL,
+	"title"	TEXT NOT NULL,
+	"status"	INTEGER NOT NULL,
+	"val1"	INTEGER NOT NULL,
+	"val2"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
